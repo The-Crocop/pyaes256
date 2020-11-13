4642152da67f372ed0ebb9ac86a8bd3d4b11a982
Marko Nalis <marko@nalis.dev>
2020-11-13 23:59:54 +0100

    Improve packaging

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(name='pyaes256',
       version='0.1',
+      install_requires=['markdown', 'pillow', 'qrcode', 'weasyprint'],
+      py_modules=['pyaes256'],
+      packages=['pyaes256'],
       entry_points={
           'console_scripts': ['pyaes256=pyaes256.__main__:run']
       }
