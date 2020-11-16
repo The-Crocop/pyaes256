4b1cd7a6d05b134d41d2d0fb89937f9f61e61fb7
Marko Nalis <marko@nalis.dev>
2020-11-16 16:08:27 +0100

    fix QR code generation issue added output and salt params added github actions file

--- pyaes256/__main__.py
+++ pyaes256/__main__.py
@@ -7,30 +7,48 @@ from pyaes256.encrypt import encrypt
 from pyaes256.paper_wallet import generate_paper_wallet
 
 
+def validate_salt(value):
+    if len(value) != 16:
+        raise argparse.ArgumentTypeError("Salt must be a 16 characters long (hex)")
+    return value
+
+
 def run():
     parser = argparse.ArgumentParser(description='Encrypt and decrypt text with AES-256 ECB and pbkdf2 as base64 like openssl')
-    parser.add_argument('--input',
+    required_args = parser.add_argument_group('required named arguments')
+    parser.add_argument('action', choices=['encrypt', 'decrypt'])
+    required_args.add_argument('-i', '--input',
                            metavar='input',
                            type=str,
                            required=True,
                            help='the text to encrypt/decrypt depending on the mode')
 
-    parser.add_argument('--password',
-                           metavar='input',
+    required_args.add_argument('-p', '--password',
+                           metavar='password',
                            type=str,
                            required=True,
                            help='the password')
-    parser.add_argument('action', choices=['encrypt', 'decrypt'])
+    parser.add_argument('-o', '--output',
+                        metavar='output',
+                        type=str,
+                        required=False,
+                        help='target filename and path')
+    parser.add_argument('-s', '--salt',
+                        metavar='salt',
+                        type=validate_salt,
+                        required=False,
+                        help='you can optionally specify a salt (in hex format) 16 characters')
 
     args = parser.parse_args()
     if 'encrypt' == args.action:
-        encryptedText = encrypt(args.input, args.password.encode())
-        print(f"Encrypted: {encryptedText}")
-        generate_paper_wallet(encryptedText)
+        print('encrypting with aes256...')
+        encrypted_text = encrypt(args.input, args.password.encode(), args.salt)
+        print(f"Encrypted: {encrypted_text}")
+        generate_paper_wallet(encrypted_text, output_file=args.output)
 
     elif 'decrypt' == args.action:
-        decryptedText = decrypt(args.input, args.password.encode())
-        print(f"Decrypted: {decryptedText}")
+        decrypted_text = decrypt(args.input, args.password.encode())
+        print(f"Decrypted: {decrypted_text}")
 
 
 if __name__ == "__main__":
--- pyaes256/paper_wallet.py
+++ pyaes256/paper_wallet.py
@@ -3,70 +3,64 @@ import os
 from io import BytesIO
 
 import qrcode
+import tempfile
 from string import Template
 from weasyprint import HTML
 import markdown as markdown
-import pathlib
-
-
-def pil2datauri(img):
-    """
-    converts PIL image to datauri
-    :param img: PIL image
-    :return: data uri string
-    """
-    data = BytesIO()
-    img.save(data, "png")
-    data64 = base64.b64encode(data.getvalue())
-    return u'data:img/png;base64,'+data64.decode('utf-8')
 
 
 def generate_paper_wallet(cyphertext, output_file='output/paperKey.pdf'):
+
     """
     generates a pdf containing a QR Code with the cyphertext, the cyphertext as text and explanations on how to decrypt
     :param cyphertext:
     :param output_file:
     :return:
     """
-    qr_output_path = pathlib.Path().absolute() / 'output'
-    qr_output_path.mkdir(parents=True, exist_ok=True)
+    if output_file is None:
+        output_file = 'output/paperKey.pdf'
 
-    img = qrcode.make(cyphertext).resize((250, 250))
-    img_data_uri=pil2datauri(img)
-    d = {
-        'cyphertext': cyphertext,
-        'aesMode': 'AES-256 ECB PBKDF2',
-        'qrCodeFile': img_data_uri
-    }
-    with open(os.path.join(os.path.dirname(__file__), 'templates/printTemplate.md'), 'r') as templateFile:
-        with open(os.path.join(os.path.dirname(__file__), 'templates/styles.css'), mode="r", encoding="utf-8") as css_file:
+    with tempfile.NamedTemporaryFile(suffix='.png') as fp:
+        img = qrcode.make(cyphertext).resize((250, 250))
+        img.save(fp)
 
-            src = Template(templateFile.read())
-            result = src.substitute(d)
-            input_html = markdown.markdown(
-                result, extensions=["markdown.extensions.abbr",
-                                    "markdown.extensions.attr_list",
-                                    "markdown.extensions.def_list",
-                                    "markdown.extensions.fenced_code",
-                                    "markdown.extensions.footnotes",
-                                    "markdown.extensions.tables",
-                                    "markdown.extensions.md_in_html"]
-            )
-            css_input = css_file.read()
+        d = {
+            'cyphertext': cyphertext,
+            'aesMode': 'AES-256 ECB PBKDF2',
+            'qrCodeFile': fp.name
+        }
+        with open(os.path.join(os.path.dirname(__file__), 'templates/printTemplate.md'), 'r') as templateFile:
+            with open(os.path.join(os.path.dirname(__file__), 'templates/styles.css'), mode="r", encoding="utf-8") as css_file:
 
-            output_html = f"""
-                <html>
-                    <head>
-                        <style>{css_input}</style>
-                    </head>
-                    <body class="markdown-body">{input_html}</body>
-                </html>
-                """
-            html = HTML(string=output_html, base_url='output')
+                src = Template(templateFile.read())
+                result = src.substitute(d)
+                input_html = markdown.markdown(
+                    result, extensions=["markdown.extensions.abbr",
+                                        "markdown.extensions.attr_list",
+                                        "markdown.extensions.def_list",
+                                        "markdown.extensions.fenced_code",
+                                        "markdown.extensions.footnotes",
+                                        "markdown.extensions.tables",
+                                        "markdown.extensions.md_in_html"]
+                )
+                css_input = css_file.read()
 
-            html.write_pdf(output_file)
-            # opener ="open" if sys.platform == "darwin" else "xdg-open"
-            # subprocess.call([opener, paperKeyFile])
+                output_html = f"""
+                    <html>
+                        <head>
+                            <style>{css_input}</style>
+                        </head>
+                        <body class="markdown-body">{input_html}</body>
+                    </html>
+                    """
+                html = HTML(string=output_html, base_url='output')
 
-            # targetFile = open("output/paperKey.html", "w", encoding="utf-8", errors="xmlcharrefreplace")
-            # targetFile.write(result)
+                html.write_pdf(output_file)
+                print("--------------------")
+                print(f"encrypted paper generated in\n file://{os.path.abspath(output_file)}")
+                # opener ="open" if sys.platform == "darwin" else "xdg-open"
+                # subprocess.call([opener, paperKeyFile])
+
+                # targetFile = open("output/paperKey.html", "w", encoding="utf-8", errors="xmlcharrefreplace")
+                # targetFile.write(result)
+
