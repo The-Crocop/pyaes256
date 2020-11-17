03d423f1a880334928405d464b5d872237c746c2
Marko Nalis <marko@nalis.dev>
2020-11-17 15:50:30 +0100

    Use custom temp file instead of python builtin, because of tons of issues in windows

--- pyaes256/paper_wallet.py
+++ pyaes256/paper_wallet.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import os
 
 import qrcode
-import tempfile
 from string import Template
 from weasyprint import HTML
 import markdown as markdown
@@ -18,51 +17,52 @@ def generate_paper_wallet(cyphertext, output_file='output/paperKey.pdf'):
     now = datetime.now()  # generate timestamp for file generation and to show in generated pdf
 
     if output_file is None:
-
         output_file = f'paperKey_{now.strftime("%Y%m%d%H%M%S")}.pdf'
 
-    qr_code_filename = os.path.join(tempfile.gettempdir(), f"{os.urandom(24).hex()}.png")
-    with open(qr_code_filename, 'wb') as fp:
-        img = qrcode.make(cyphertext).resize((250, 250))
-        img.save(fp)
+    #  NamedTempFile has issues in windows, as does using an absolute directory and referencing it in markdown.
+    #  Therefore we handle creation and deletion of the file ourselves
+    qr_tmp_filename = f"{os.urandom(24).hex()}.png"
 
-        d = {
-            'cyphertext': cyphertext,
-            'aesMode': 'AES-256 ECB PBKDF2',
-            'qrCodeFile': fp.name,
-            'generationDateTime': now.strftime("%m/%d/%Y, %H:%M:%S")
-        }
-        with open(os.path.join(os.path.dirname(__file__), 'templates/printTemplate.md'), 'r') as templateFile:
-            with open(os.path.join(os.path.dirname(__file__), 'templates/styles.css'), mode="r", encoding="utf-8") as css_file:
+    try:
+        with open(qr_tmp_filename, 'wb') as fp:
+            img = qrcode.make(cyphertext).resize((250, 250))
+            img.save(fp)
 
-                src = Template(templateFile.read())
-                result = src.substitute(d)
-                input_html = markdown.markdown(
-                    result, extensions=["markdown.extensions.abbr",
-                                        "markdown.extensions.attr_list",
-                                        "markdown.extensions.def_list",
-                                        "markdown.extensions.fenced_code",
-                                        "markdown.extensions.footnotes",
-                                        "markdown.extensions.tables",
-                                        "markdown.extensions.md_in_html"]
-                )
-                css_input = css_file.read()
+            d = {
+                'cyphertext': cyphertext,
+                'aesMode': 'AES-256 ECB PBKDF2',
+                'qrCodeFile': fp.name,
+                'generationDateTime': now.strftime("%m/%d/%Y, %H:%M:%S")
+            }
+            with open(os.path.join(os.path.dirname(__file__), 'templates/printTemplate.md'), 'r') as templateFile:
+                with open(os.path.join(os.path.dirname(__file__), 'templates/styles.css'), mode="r", encoding="utf-8") as css_file:
 
-                output_html = f"""
-                    <html>
-                        <head>
-                            <style>{css_input}</style>
-                        </head>
-                        <body class="markdown-body">{input_html}</body>
-                    </html>
-                    """
-                html = HTML(string=output_html, base_url='output')
+                    src = Template(templateFile.read())
+                    result = src.substitute(d)
+                    input_html = markdown.markdown(
+                        result, extensions=["markdown.extensions.abbr",
+                                            "markdown.extensions.attr_list",
+                                            "markdown.extensions.def_list",
+                                            "markdown.extensions.fenced_code",
+                                            "markdown.extensions.footnotes",
+                                            "markdown.extensions.tables",
+                                            "markdown.extensions.md_in_html"]
+                    )
+                    css_input = css_file.read()
 
-                html.write_pdf(output_file)
-                print("--------------------")
-                print(f"encrypted paper generated in\n file://{os.path.abspath(output_file)}")
-                # opener ="open" if sys.platform == "darwin" else "xdg-open"
-                # subprocess.call([opener, paperKeyFile])
+                    output_html = f"""
+                        <html>
+                            <head>
+                                <style>{css_input}</style>
+                            </head>
+                            <body class="markdown-body">{input_html}</body>
+                        </html>
+                        """
+                    html = HTML(string=output_html, base_url='output')
 
-                # targetFile = open("output/paperKey.html", "w", encoding="utf-8", errors="xmlcharrefreplace")
-                # targetFile.write(result)
+                    html.write_pdf(output_file)
+                    print("--------------------")
+                    print(f"encrypted paper generated in\n file://{os.path.abspath(output_file)}")
+
+    finally:
+        os.remove(qr_tmp_filename)
