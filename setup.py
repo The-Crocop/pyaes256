026f0c24855c14639e8c8b7afb912d31645be8d5
Marko Nalis <marko@nalis.dev>
2020-11-17 18:00:08 +0100

    Use custom temp file instead of python builtin, because of tons of issues in windows

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(name='pyaes256',
-      version='0.1',
+      version=get_version("pyaes256/__init__.py"),
       install_requires=['markdown', 'pillow', 'qrcode', 'weasyprint', 'cairocffi', 'pycryptodome'],
       author='Marko Nalis',
-      author_email="author@email.com",
+      author_email="marko@nalis.dev",
       url='https://github.com/The-Crocop/pyaes256',
       packages=find_packages(),
       include_package_data=True,
